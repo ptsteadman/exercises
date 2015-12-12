80bade9af58ff5ada581dbc645433d18cd55ebb3
Patrick Steadman <ptsteadman@gmail.com>
2015-12-12 14:26:23 -0500

    Add inputs for interleave strings

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-aws s3 sync exercises/ s3://exercises.ptsteadman.com --region us-east-1 --acl public-read --exclude .git/*
+aws s3 sync . s3://exercises.ptsteadman.com --region us-east-1 --acl public-read 
